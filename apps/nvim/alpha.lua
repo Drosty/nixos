ffb59d9e9c2717d8d6c5796125840455b3b6ed91
Ryan Drost <ryan@drostdev.com>
2024-04-11 22:21:48 -0500

    remove comments

--- apps/nvim/alpha.lua
+++ apps/nvim/alpha.lua
@@ -1,10 +1,4 @@
--- return {
---  'goolord/alpha-nvim',
---  dependencies = { 'nvim-tree/nvim-web-devicons' },
---  config = function ()
-    local alpha = require('alpha')
-    local startify = require("alpha.themes.startify")
-    alpha.setup(startify.config)
---  end
---};
+local alpha = require('alpha')
+local startify = require("alpha.themes.startify")
+alpha.setup(startify.config)
 
